0a8e94787f9e4abe96329003ed1d03c5e05822c3
Lim Shu Yi <shuyi1480@gmail.com>
2024-08-14 16:19:56 +0800

    Change screen window resolution

--- shuyi/shuyi.py
+++ shuyi/shuyi.py
@@ -2,7 +2,7 @@ import pygame
 
 # Initialize Pygame
 pygame.init()
-screen = pygame.display.set_mode((1280, 768))
+screen = pygame.display.set_mode((1920, 1080))
 pygame.display.set_caption("Simulator Game")
 
 # Colors
