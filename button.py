077a57509cc83bea193e39ab02802c55bebf84c0
sayyun886 <sayyuntoon@gmail.com>
2024-09-02 09:49:42 +0800

    eleventh ver

--- button.py
+++ button.py
@@ -4,7 +4,7 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((1280, 720))
-pygame.display.set_caption("Pet Interaction Game")
+pygame.display.set_caption("Love Button Interaction")
 
 images = [
     pygame.transform.scale(pygame.image.load("love button/dog_i.png"), (1280, 720)), 
@@ -25,6 +25,7 @@ interaction_area = pygame.Rect(300, 250, 400, 200)
 last_mouse_x = None
 direction = None
 strokes = 0
+#count the num within interact area
 
 running = True
 
@@ -39,7 +40,7 @@ while running:
         if event.type == pygame.MOUSEMOTION:
             mouse_x, mouse_y = event.pos
             
-            # Check the mouse is in the interaction area
+            # "Colli" check the mouse is in the interaction area
             if interaction_area.collidepoint(mouse_x, mouse_y):
                 if last_mouse_x is not None:
                     if mouse_x > last_mouse_x and direction != "right":
@@ -66,9 +67,11 @@ while running:
 
     screen.blit(images[current_image_index], (0, 0))
 
+#bar column
     pygame.draw.rect(screen, (0, 0, 0), (bar_x, bar_y, bar_width, bar_height), 2)  # Bar outline
     pygame.draw.rect(screen, (0, 255, 0), (bar_x, bar_y, (progress / 100) * bar_width, bar_height))  # Progress fill
 
+#red red column
     pygame.draw.rect(screen, (255, 0, 0, 100), interaction_area, 2)  # Red rectangle for the interaction area
     
     pygame.display.flip()
