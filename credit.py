fac725415503aacad284ee39022c3a5b24aa8fd2
Lim Shu Yi <shuyi1480@gmail.com>
2024-09-27 20:05:19 +0800

    cleared all bug, settled unused code

--- credit.py
+++ credit.py
@@ -12,29 +12,13 @@ pygame.display.set_icon(icon)
 
 credit_background = pygame.image.load("asset/image/creditimg.png")
 
-# text that will be shown in this page
-# (credsha, credsha_rect), (cred, cred_rect) = common.text_with_shadow("CREDITS", common.monera(150), "Black", "White", (640, 120))
-# (mmus, mmus_rect), (mmu, mmu_rect) = common.text_with_shadow("Our Best Lecture", common.playfair(80), "Black", "White", (640, 345))
-# (mrw, mrw_rect) = common.normal_text("Mr Willie", common.playfair(80), "White", pos=(640, 495))
-# (credsha, credsha_rect), (cred, cred_rect) = common.text_with_shadow("CREDITS", common.monera(150), "Black", "White", (640, 120))
-# (mmus, mmus_rect), (mmu, mmu_rect) = common.text_with_shadow("Our Best Lecture", common.playfair(80), "Black", "White", (640, 345))
-# (mrw, mrw_rect) = common.normal_text("Mr Willie", common.playfair(80), "White", pos=(640, 495))
-
-
 # back to menu img and button
 return_img = pygame.image.load("asset/image/return.png")
 return_button = Button(50, 50, image=return_img, scale=0.27)
 
 
 def credits(event):
-    # print background and texts
     screen.blit(credit_background, (0, 0))
-    # screen.blit(credsha, credsha_rect)
-    # screen.blit(cred, cred_rect)
-    # screen.blit(mmus, mmus_rect)
-    # screen.blit(mmu, mmu_rect)
-    # screen.blit(mrw, mrw_rect)
-
     if event.type == pygame.QUIT:
         common.running = False  # Quit the game
         return  # return to main loop
